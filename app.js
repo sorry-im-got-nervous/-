9c693f18e5eed183952b30b091074ef06d909971
sorry-im-got-nervous <117665318+sorry-im-got-nervous@users.noreply.github.com>
2024-06-27 19:18:13 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+let PlanComponent = {
+  template: '#plan-template',
+  props: {
+    name: { type: String, required: true },
+    selected: { type: Boolean, default: false },
+  },
+  methods:{
+    select(){
+      this.$emit('select', this.name)
+    }
+  }
+}
+
+let PlanPickerComponent = {
+  components: { plan: PlanComponent },
+  template: '#plan-picker-template',
+  data(){
+    return {
+      plans: ['The Single', 'The Curious', 'The Addict'],
+      selectedPlan: null
+    }
+  },
+  methods:{
+    selectPlan(plan){
+      this.selectedPlan = plan      
+    }
+  }
+  }
+
+const app = Vue.createApp({
+  components: { PlanPicker: PlanPickerComponent }
+})
+.mount('#app')
